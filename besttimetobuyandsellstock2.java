dad03761523f43162037397e88edc6d4a1498c81
pulkitjhod <pulkit.kumar2020@vitbhopal.ac.in>
2023-01-08 10:12:12 +0530

    initial commit

--- besttimetobuyandsellstock2.java
+++ besttimetobuyandsellstock2.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 class besttimetobuyandsellstock2 {
-    // code 
+    // code new
     public static int maxProfit(int[] prices) {
         int n = prices.length;
         int profit =0;
